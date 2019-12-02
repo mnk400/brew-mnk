8659ec659cfcb7e28dcf433679853350dac48502
Manik <mnk_400@icloud.com>
2019-12-01 19:30:36 -0500

    init

--- piholeinfo.rb
+++ piholeinfo.rb
@@ -1,4 +1,4 @@
-class Piholeinfo < formula
+class Piholeinfo < Formula
     desc "A simple script that displays your pi.hole stats in terminal"
     homepage "https://github.com/mnk400/pihole-info"
     url "https://github.com/mnk400/pihole-info/blob/master/dist/pihole-info/piholeinfo"
